9db7f7885e537825dd359f793c65951856918ce2
Phoenix-Nye <phoenix.nye@gmail.com>
2024-08-19 12:14:36 -0700

    updated user input

--- src/new-game/main.ts
+++ src/new-game/main.ts
@@ -15,16 +15,19 @@ type Vector2 = { x: number, y: number };
 type Player = {
     type: "player",
     position: Vector2,
+    controls: { up: string, down: string, left: string, right: string, shoot: string},
+    speed: number,
 }
 
-type Ball = {
-    type: "ball",
+type Bullet = {
+    type: "bullet",
     position: Vector2,
     size: Vector2,
     color: string,
+    bulletspeed: number
 }
 
-type Entity = Player | Ball;
+type Entity = Player | Bullet;
 
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -33,9 +36,9 @@ type Entity = Player | Ball;
 //////////////////////////////////////////////////////////////////////////////////////////
 
 const displayList: Entity[] = [
-    { type: "player", position: { x: 10, y: 10 }},
-    { type: "ball", position: { x: 120, y: 20 }, size: { x: 10, y: 10 }, color: "red"},
-    { type: "ball", position: { x: 120, y: 60 }, size: { x: 10, y: 10 }, color: "blue"},
+    { type: "player", position: { x: 10, y: 10 }, controls: { up: "w", left:"a", down: "s", right: "d", shoot: " "}, speed: 1 }//,
+    //{ type: "ball", position: { x: 120, y: 20 }, size: { x: 10, y: 10 }, color: "red"},
+    //{ type: "ball", position: { x: 120, y: 60 }, size: { x: 10, y: 10 }, color: "blue"},
 ]
 
 
@@ -58,8 +61,30 @@ document.body.addEventListener("keyup", (event: KeyboardEvent) => {
 function updateState() {
     for (let entity of displayList) {
         if (entity.type == "player") {
+            // check if players keys are down and move
+            if (keyState[entity.controls.right]) {
+                entity.position.x += entity.speed;
+            }
+            if (keyState[entity.controls.left]) {
+                entity.position.x -= entity.speed;
+            }
+            if (keyState[entity.controls.down]) {
+                entity.position.y += entity.speed;
+            }
+            if (keyState[entity.controls.up]) {
+                entity.position.y -= entity.speed;
+            }
+            if (keyState[entity.controls.shoot]) {
+                displayList.push ({
+                    type: "bullet",
+                    "color": "orange",
+                    "position": {x:10, y:10},
+                    "size": {x: 10, y:10},
+                    "bulletspeed": 1
+                })
+            }
         }
-        else if (entity.type == "ball") {
+        else if (entity.type == "bullet") {
 
         }
     }
@@ -68,13 +93,12 @@ function updateState() {
 function renderState() {
     // Clear the screen
     c.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-
     for (let entity of displayList) {
         if (entity.type == "player") {
             c.fillStyle = "purple";
-            c.fillRect(entity.position.x, entity.position.y, 20, 40);
+            c.fillRect(entity.position.x, entity.position.y, 30, 30);
         }
-        else if (entity.type == "ball") {            
+        else if (entity.type == "bullet") {            
             c.fillStyle = entity.color;
             c.fillRect(entity.position.x, entity.position.y, entity.size.x, entity.size.y);
         }
